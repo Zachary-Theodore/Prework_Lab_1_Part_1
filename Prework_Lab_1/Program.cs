ee8e5f3ce96241f9b4626b2c2b731f4c34b53335
Zachary Theodore <fk9836@wayne.edu>
2018-06-11 17:10:23 -0400

    Prework part 1 for lab 1

---
+++ Prework_Lab_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prework_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input two numbers of the same length.");
+            Console.WriteLine("Number one: ");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Number two: ");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("You entered " + num1 + " And " + num2);
+
+            while (num1.Length != num2.Length)
+            {
+                Console.WriteLine("The numbers you have entered are not of equal length.");
+                Console.WriteLine("Input two numbers of the same length.");
+                Console.WriteLine("Number one: ");
+                num1 = Console.ReadLine();
+                Console.WriteLine("Number two: ");
+                num2 = Console.ReadLine();
+                Console.WriteLine("You entered " + num1 + " And " + num2);
+            }
+
+
+
+
+
+
+
+            for (int i = 0; i <= num1.Length - 1; i++)
+            {
+                if (i == num1.Length - 1)
+                {
+                    Console.WriteLine("True");
+                    break;
+                }
+                else if (num1[i] + num2[i] != num1[i + 1] + num2[i + 1])
+                {
+                    Console.WriteLine("False");
+                    break;
+                }
+
+
+            }
+            Console.WriteLine("Thank You!!!");
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
+    
+
