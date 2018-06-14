8ed32ea8092b6d663c6c1493f099cd7d53b3e8a6
Zachary_Theodore <fk9836@wayne.edu>
2018-06-14 17:42:29 -0400

    Updated version of project 1

--- Prework_Lab_1/Program.cs
+++ Prework_Lab_1/Program.cs
@@ -10,14 +10,18 @@ namespace Prework_Lab_1
     {
         static void Main(string[] args)
         {
+            // asks for user to input two numbers of the same length
             Console.WriteLine("Input two numbers of the same length.");
             Console.WriteLine("Number one: ");
             string num1 = Console.ReadLine();
             Console.WriteLine("Number two: ");
             string num2 = Console.ReadLine();
 
+            //displays what was entered
             Console.WriteLine("You entered " + num1 + " And " + num2);
 
+
+            //checks at whether or not the user inputs are of equal value and if not asks to input again
             while (num1.Length != num2.Length)
             {
                 Console.WriteLine("The numbers you have entered are not of equal length.");
@@ -34,7 +38,7 @@ namespace Prework_Lab_1
 
 
 
-
+            // this loop makes the decision on whether the numbers are equal or not and outputs true or false 
             for (int i = 0; i <= num1.Length - 1; i++)
             {
                 if (i == num1.Length - 1)
@@ -50,6 +54,7 @@ namespace Prework_Lab_1
 
 
             }
+            //closing comment and waits for user to to press enter before exiting
             Console.WriteLine("Thank You!!!");
             Console.ReadKey();
         }
